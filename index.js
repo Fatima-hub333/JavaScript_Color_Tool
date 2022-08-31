49ae63890a5457cc1e8e5752691b66939b6aa69d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 21:40:00 +0500

    Convert HEX to RGBA

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ const isValidHex = (hex) => {
   return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
+// Second Challenge
+//Create a function to convert Hex to RGB
+const convertHexToRGB = (hex) => {
+  if (!isValidHex(hex)) return null;
+  let strippedHex = hex.replace('#', '');
+  //this should work with 3 or 6 character hex values
+  if (strippedHex.length === 3) {
+    strippedHex = strippedHex[0] + strippedHex[0]
+      + strippedHex[1] + strippedHex[1]
+      + strippedHex[2] + strippedHex[2];
+  }
+  //Hint - use parseInt("", 16) to convert a hex value to a decimal value
+  //should return an object with 3 properties - r,g, and b
+  const r = parseInt(strippedHex.substring(0, 2), 16);
+  const g = parseInt(strippedHex.substring(2, 4), 16);
+  const b = parseInt(strippedHex.substring(4, 6), 16);
+
+  return { r, g, b };
+}
+
+console.log (convertHexToRGB("eee"));
