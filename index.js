90b0cb7a39bbf4e80530340054d6ecc9e84ede0f
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 21:21:01 +0500

    Display Color from User Hex input

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-//Check to see whether the input from the user is a valid hex color
+//Get a reference to hexInput and inputColor DOM elements
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+//Create a keyup event handler for hexInput
+hexInput.addEventListener('keyup', () => {
+  const hex = hexInput.value;
+  //Check if hex color is valid
+  if(!isValidHex(hex)) return;
+  
+//If hex color is valid, update the background color of inputColor
+  const strippedHex = hex.replace('#', '');
+  inputColor.style.backgroundColor = "#" + strippedHex;
+})
 
 const isValidHex = (hex) => {
-  if (!hex) return false;
-
+  if(!hex) return false;
+  
   const strippedHex = hex.replace('#', '');
   return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
-console.log(isValidHex("#000000")) //true
-console.log(isValidHex("#0000000")) //false
-console.log(isValidHex("#ffffff")) //true
-console.log(isValidHex("#fff")) //true
